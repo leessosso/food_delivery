d4a9f8b37c4c30995e37d4f271deea4c260c0cb2
leessosso <ssossoking@naver.com>
2022-03-03 22:39:25 +0900

    first commit

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  _MainFoodPageState createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
