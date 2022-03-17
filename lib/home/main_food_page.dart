e3104026e319ef857ea1a572f509f9c9e5b22a3c
leessosso <ssossoking@naver.com>
2022-03-17 21:49:38 +0900

    220317 start food_page_body.dart

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -4,6 +4,8 @@ import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
 
+import 'food_page_body.dart';
+
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({Key? key}) : super(key: key);
 
@@ -30,10 +32,15 @@ class _MainFoodPageState extends State<MainFoodPage> {
                         text: "Korea",
                         color: AppColors.mainColor,
                       ),
-                      SmallText(
-                        text: 'Seoul',
-                        color: Colors.black54,
-                      ),
+                      Row(
+                        children: [
+                          SmallText(
+                            text: 'Seoul',
+                            color: Colors.black54,
+                          ),
+                          Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
                     ],
                   ),
                   Center(
@@ -50,7 +57,8 @@ class _MainFoodPageState extends State<MainFoodPage> {
                 ],
               ),
             ),
-          )
+          ),
+          FoodPageBody()
         ],
       ),
     );
